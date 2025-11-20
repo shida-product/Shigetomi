e830f0e3aa61f0a4c69fe4fac45c6885d287e3c6
shida-product <shida.kazuto.95@gmail.com>
2025-11-20 20:38:29 +0900

    Delete src directory

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { LanguageSelectorPattern1 } from './components/LanguageSelectorPattern1';
-
-export default function App() {
-  return <LanguageSelectorPattern1 />;
-}
